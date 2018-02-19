00e3cf46103e3697314f37a43572814aed3629a2
Kristian Lein-Mathisen <kristianlein@gmail.com>
2018-02-19 22:01:00 +0100

    fix and cleanup: example-hello-world
    - bugfix: reference y-coordinate too!
    - ui: no flickering with reset-cursor (instead of cls on every frame)

--- simple/example-hello-world.scm
+++ simple/example-hello-world.scm
@@ -1,35 +1,39 @@
-(use miscmacros ports)
-(load "enum-pixfmts.so")
-(load "ffmpeg.so")
+(use ports ffmpeg)
 
+(define (cls) (display "\x1b[2J"))
+(define (reset-cursor) (display "\x1b[u"))
+
+; TODO: make it pretty, like this
+;; (define-flg
+;;   (in "testsrc" "size=100x30")
+;;   (out "buffersink" pix-fmts: '(gray8)))
 (begin
   (define fg    (make-flg))
-  (define in    (make-flx fg (find-filter "testsrc")    "in"))
+  (define in    (make-flx fg (find-filter "testsrc")    "in" "size=100x30"))
   (define out   (make-flx fg (find-filter "buffersink") "out"))
-  (define scale (make-flx fg (find-filter "scale")      "scale"))
-
-  (avfilter-init-str in "size=1024x840")
   (opt-set-pix-fmts out '(gray8))
-  (avfilter-init-str scale "100x35")
 
-  (avfilter-link in 0 scale 0)
-  (avfilter-link scale 0 out 0)
+  (avfilter-link in 0 out 0)
   (avfilter-graph-config fg))
 
 (define (display-frame frame)
   (let ((data (frame-data frame 0)))
     (do ((y 0 (+ y 1)))
-        ((> y (frame-height frame)))
+        ((>= y (frame-height frame)))
       (do ((x 0 (+ x 1)))
-          ((> x (frame-width frame)))
-        (print* (string-ref " .-+#" (inexact->exact
-                                     (floor (/ (u8vector-ref data x) 52))))))
-      (print)))
-  (thread-sleep! 0.1))
+          ((>= x (frame-width frame)))
+        (display (string-ref
+                  " .-+#"
+                  (inexact->exact
+                   (floor (/ (u8vector-ref
+                              data
+                              (+ (* (frame-linesize frame 0) y) x)) 52))))))
+      (print))))
 
+(cls)
 (port-for-each
  (lambda (frame)
-   (print "\033c")
+   (reset-cursor)
    (print "frame: " frame "  " (frame-pts frame))
    (display-frame frame))
  (let ((frame (make-frame)))
@@ -37,12 +41,3 @@
      (frame-unref frame)
      (av-buffersink-get-frame out frame))))
 
-(define fmx (avformat-open-input "" "x11grab"))
-(define stm (fmtx-stream fmx 0))
-(define cox (codecx stm))
-
-(define pkt (make-packet))
-(fmtx-read fmx pkt)
-
-(avcodec-send-packet cox pkt)
-(avcodec-receive-frame cox frame)
