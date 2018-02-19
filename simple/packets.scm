14121f54dcd0fa923b9eb7611822cbd2810f8088
Kristian Lein-Mathisen <kristianlein@gmail.com>
2018-02-19 22:04:53 +0100

    examples: cleanup

--- simple/packets.scm
+++ simple/packets.scm
@@ -1,13 +1,15 @@
 (use ports ffmpeg)
-;;(load "enum-pixfmts.so")
-;;(load "ffmpeg.so")
 
-(define fmtx (avformat-open-input (list-ref (command-line-arguments) 0)))
-(print fmtx)
+(unless (= 1 (length (command-line-arguments)))
+  (print "usage: csi -s packets.scm <media-file>
+  reads all packets and print their info"))
+
+(define fmx (avformat-open-input (list-ref (command-line-arguments) 0)))
+(print "fmx: " fmx)
 (port-for-each
  (lambda (pkt)
    (print "pkt: " pkt))
  (let ((pkt (make-packet)))
    (lambda ()
      (packet-unref pkt) ;; unref previous instance, otherwise we leak
-     (fmtx-read fmtx pkt))))
+     (fmtx-read fmx pkt))))
